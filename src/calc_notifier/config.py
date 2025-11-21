0d89684f77ee5e901b6c4245ebc3b5c12181ea52
helloginov <70898926+helloginov@users.noreply.github.com>
2025-11-21 11:27:17 +0300

    Clear Telegram token and chat ID in config
    Removed sensitive Telegram token and chat ID.

--- src/calc_notifier/config.py
+++ src/calc_notifier/config.py
@@ -1,8 +1,8 @@
 CONFIG = {
     "telegram": {
         "enabled": True,
-        "token": "8051637976:AAH_gBzC7bNIGlnZcUg6P22ZFoQQ3GkExyc",
-        "chat_id": "382533542"
+        "token": "",
+        "chat_id": ""
       },
     "history_dir": "./calc_notifier_history",
     "keep_last": 3
